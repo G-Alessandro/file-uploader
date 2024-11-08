3f6d0b42e642646688efa9c030c3b04ddfd52ebc
Alessandro <galloalessandro77@gmail.com>
2024-11-08 17:37:26 +0100

    Added passport-config.js

---
+++ backend/utils/user-authentication/passport-config.js
@@ -0,0 +1,56 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+passport.use(
+  new LocalStrategy(
+    { usernameField: "email", passwordField: "password" },
+    async (email, password, done) => {
+      try {
+        const user = await prisma.userAccount.findUnique({
+          where: {
+            email: email,
+          },
+        });
+
+        if (!user) {
+          return done(null, false, {
+            message: "Account does not exist or incorrect email",
+          });
+        }
+
+        const match = await bcrypt.compare(password, user.password);
+
+        if (!match) {
+          return done(null, false, { message: "Incorrect password" });
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await prisma.userAccount.findUnique({ where: { id: id } });
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+process.on("SIGINT", async () => {
+  await prisma.$disconnect();
+  process.exit(0);
+});
+
+module.exports = passport;
